a42b0fbace6293bbbd39b004b6b4410902d8c99e
RalfKeller <ralf.keller1997@gmail.com>
2017-06-16 20:52:25 +0200

    Wuush

--- smarthair/src/app/packetComponent/packet.component.ts
+++ smarthair/src/app/packetComponent/packet.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { routerTransition } from '../animation/router.animations'
 
 @Component({
     selector: 'packet-component',
-    templateUrl: 'packet.component.html'
+    templateUrl: 'packet.component.html',
+    animations: [routerTransition()],
+    host: {'[@routerTransition]' : ''}
 })
 
 export class PacketComponent implements OnInit {
--- smarthair/src/app/plzComponent/plz.component.ts
+++ smarthair/src/app/plzComponent/plz.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { routerTransition } from '../animation/router.animations'
 
 @Component({
     selector: 'plz',
-    templateUrl: 'plz.component.html'
+    templateUrl: 'plz.component.html',
+    animations: [routerTransition()],
+    host: {'[@routerTransition]' : ''}
 })
 
 export class PlzComponent implements OnInit {
